0f0d439f455035cfe87f633c8ccc5b0a195f1843
ryger <vartiainen.henri@gmail.com>
2021-09-04 22:32:02 +0300

    Remove classes blog and post

--- Core/Data/ApplicationDbContext.cs
+++ Core/Data/ApplicationDbContext.cs
@@ -15,22 +15,4 @@ namespace TeachTips.Core.Data
 
         }
     }
-
-    public class Blog
-    {
-        public int BlogId { get; set; }
-        public string Url { get; set; }
-
-        public List<Post> Posts { get; set; }
-    }
-
-    public class Post
-    {
-        public int PostId { get; set; }
-        public string Title { get; set; }
-        public string Content { get; set; }
-
-        public int BlogId { get; set; }
-        public Blog Blog { get; set; }
-    }
 }
